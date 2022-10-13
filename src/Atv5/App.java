3093cb2acff9775f120fe38de5886984c30a534a
GIL <gil.00000032610@unicap.br>
2022-10-13 19:17:21 -0300

    senha had to go

--- src/Atv5/App.java
+++ src/Atv5/App.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 public class App {
    public static void main(String[] args) {
       float renda;
+      double value;
       Cliente client;
       Scanner s = new Scanner(System.in);
       ContaBancaria cb;
       String name, email, phone, RG, senha;
       ContaBancaria conta;
-      int tipo;
+      int tipo, op;
 
       try {
          System.out.println("Informe o nome: ");
@@ -27,7 +28,42 @@ public class App {
          client = new Cliente(name, email, phone, RG, renda);
          System.out.println("Informe o tipo de conta: ");
          tipo = Integer.parseInt(s.nextLine());
-         
+
+         System.out.println("Informe o tipo da conta: ");
+         tipo = Integer.parseInt(s.nextLine());
+
+         System.out.println("Informe a nova senha: ");
+         senha = s.nextLine();
+         conta = new ContaBancaria(tipo, senha, client);
+
+         do {
+            showMenu();
+            op = Integer.parseInt(s.nextLine());
+            switch (op) {
+               case 1:
+                  System.out.println("Informe o valor: ");
+                  value = Double.parseDouble(s.nextLine());
+                  conta.deposit(value);
+                  break;
+               case 2:
+                  System.out.println("Informe o valor: ");
+                  value = Double.parseDouble(s.nextLine());
+                  conta.withdraw(value);
+
+                  break;
+               case 3:
+                  conta.closeAccount();
+               }
+                  break;
+               case 0:
+                  break;
+               default:
+                  System.out.println("opção invalida");
+                  break;
+            }
+
+         } while (op != 0);
+
       } catch (InputMismatchException im) {
          System.out.println(im.getMessage());
       } catch (SaldoInsuficienteException si) {
@@ -36,4 +72,11 @@ public class App {
 
    }
 
+   public static void showMenu() {
+      System.out.println("1 - deposito");
+      System.out.println("2 - Saque");
+      System.out.println("3 - fechar conta");
+      System.out.println("0 - sair");
+
+   }
 }
--- src/Atv5/ContaBancaria.java
+++ src/Atv5/ContaBancaria.java
@@ -6,7 +6,6 @@ public class ContaBancaria {
     private String senha;
     private boolean status;
     private Cliente cliente;
-  
 
     public ContaBancaria(int tipo, String senha, Cliente cliente) {
         this.cliente = cliente;
@@ -15,7 +14,6 @@ public class ContaBancaria {
         this.senha = senha;
     }
 
-    
     public void aplicarBonus(double percentage) {
         this.saldo += this.saldo * (percentage / 100.00);
     }
@@ -24,15 +22,11 @@ public class ContaBancaria {
         this.saldo += this.saldo * (5.0 / 100.00);
     }
 
-    public void closeAccount(String senha) {
-        if (senha.equals(this.senha)) {
-            if (this.saldo > 0) {
-                System.out.println("Ainda existe dinheiro na conta. Para fechar a conta deposite todo seu saldo!");
-            } else {
-                System.out.println("Conta fechada!");
-            }
+    public void closeAccount() {
+        if (this.saldo > 0) {
+            System.out.println("Ainda existe dinheiro na conta. Para fechar a conta deposite todo seu saldo!");
         } else {
-            System.out.println("Senha incorreta!");
+            System.out.println("Conta fechada!");
         }
     }
 
@@ -40,21 +34,27 @@ public class ContaBancaria {
         this.saldo += amount;
     }
 
-    public void withdraw(Double amount, String senha) throws SaldoInsuficienteException {
-        if (this.senha.equals(senha)) {
-            if (this.saldo < amount) {
-                throw new SaldoInsuficienteException("Saldo insuficiente", null);
-            } else {
-                this.saldo -= amount;
-            }
+    public int getTipo() {
+        return this.tipo;
+    }
+
+    public void withdraw(Double amount) throws SaldoInsuficienteException {
+
+        if (this.saldo < amount) {
+            throw new SaldoInsuficienteException("Saldo insuficiente", null);
         } else {
-            System.out.println("Senha incorreta!");
+            this.saldo -= amount;
         }
+    }
+
+    public Double getSaldo() {
+        return this.saldo;
     }
 
     @Override
     public String toString() {
-        return "ContaBancaria [saldo=" + saldo + ", tipo=" + tipo+ ", senha=" + senha + ", status=" + status + ", senha="
+        return "ContaBancaria [saldo=" + saldo + ", tipo=" + tipo + ", senha=" + senha + ", status=" + status
+                + ", senha="
                 + senha + "]";
     }
 
